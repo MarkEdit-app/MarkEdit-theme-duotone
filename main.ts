0967c66f93b6d2fd88eb16e8512ddee969998d46
Ying Zhong <0x00eeee@gmail.com>
2025-07-12 12:33:30 +0800

    Better color scheme control

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-import { overrideThemes } from 'markedit-theming';
+import { CustomTheme, overrideThemes } from 'markedit-theming';
+import { MarkEdit, type JSONObject, type JSONValue } from 'markedit-api';
 import { duotoneLight, duotoneDark } from '@uiw/codemirror-theme-duotone';
 
+const toObject = (value: JSONValue, fallback = {}) => (value ?? fallback) as JSONObject;
+const rootValue = toObject(toObject(MarkEdit.userSettings)['extension.markeditThemeDuoTone']);
+const enabledMode = (rootValue.enabledMode ?? 'both') as string;
+
+const lightTheme: CustomTheme = { extension: duotoneLight };
+const darkTheme: CustomTheme = { extension: duotoneDark };
+
 overrideThemes({
-  light: { extension: duotoneLight },
-  dark: { extension: duotoneDark },
+  light: ['both', 'light'].includes(enabledMode) ? lightTheme : undefined,
+  dark: ['both', 'dark'].includes(enabledMode) ? darkTheme : undefined,
 });
