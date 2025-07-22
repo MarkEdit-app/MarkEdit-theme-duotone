8f7a18f7be5258e1bd301aaaee439e4575a89563
Ying Zhong <0x00eeee@gmail.com>
2025-07-22 21:26:05 +0800

    Integrate latest markedit-theming

--- main.ts
+++ main.ts
@@ -2,7 +2,19 @@ import { overrideThemes } from 'markedit-theming';
 import { duotoneLight, duotoneDark } from '@uiw/codemirror-theme-duotone';
 
 overrideThemes({
-  light: { extension: duotoneLight },
-  dark: { extension: duotoneDark },
-  options: { settingsKey: 'extension.markeditThemeDuoTone' },
+  light: {
+    extension: duotoneLight,
+    colors: {
+      subtleEmphasis: true,
+    },
+  },
+  dark: {
+    extension: duotoneDark,
+    colors: {
+      subtleEmphasis: true,
+    },
+  },
+  options: {
+    settingsKey: 'extension.markeditThemeDuoTone',
+  },
 });
